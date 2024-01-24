b23c3b10b11c8b84eefdb182e6e90e0b8cee08c4
Tejash Datta <tejashdatta@gmail.com>
2024-01-24 23:10:03 +0530

    Initial Commit

---
+++ German Noun Highlighter/German_Noun_HighlighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  German_Noun_HighlighterApp.swift
+//  German Noun Highlighter
+//
+//  Created by Tejash Datta on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct German_Noun_HighlighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
